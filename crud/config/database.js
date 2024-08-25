f91427d39b206e296b980ad92f5378b080b55765
ajaykumar070014 <ajaykumar070014@gmail.com>
2024-08-25 20:23:12 +0530

    changes

--- crud/config/database.js
+++ crud/config/database.js
@@ -1,8 +1,27 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize("PersonalDB", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5433,
-});
+dotenv.config();
 
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false,
+  }
+);
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection();
